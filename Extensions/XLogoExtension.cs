83a2ec88fca9682a8e82a71ccb8efe7c69eabab9
Eduardo <eduardo.romero@sap.com>
2022-09-09 17:48:47 +0300

    first commit

---
+++ Extensions/XLogoExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Writers;
+
+namespace IO.Swagger.Extensions
+{
+    public class XLogoExtension : IOpenApiExtension
+    {
+        public XLogoExtension()
+        {
+        }
+
+        public void Write(IOpenApiWriter writer, OpenApiSpecVersion specVersion)
+        {
+            writer.WriteProperty("url", "https://redocly.github.io/redoc/petstore-logo.png");
+            writer.WriteProperty("alttext", "Petstore logo");
+        }
+    }
+}
